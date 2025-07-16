bf451d83f290ce5adfb4e1c7d180fcb5f39d30d4
Muhammadamin01 <userfigma01@gmail.com>
2025-07-16 14:32:30 +0500

    Fix: missing HTML.webp asset

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,12 @@
 import '../styles/About.css';
-import Html from  "../assets/HTML.webp"
 import Css from "../assets/css.png"
 import JavaScript from "../assets/JavaScript.png"
 import Bootstrap from "../assets/Bootstrap.png"
-import TypeScript from "../assets/typescript.webp"
 import React from "../assets/React.png"
 import Vite from "../assets/Vite.png"
 import Git from "../assets/Git.png"
 import Github from "../assets/Github.png"
 import Responsive from "../assets/Responsive.png"
-import Figma from "../assets/figma.webp"
 
 const About = () => {
   return (
@@ -23,17 +20,17 @@ const About = () => {
         <div className="skills">
           <h3>My knowledge</h3>
           <ul className="skill-list">
-            <li><img src={Html} alt="html" /> HTML</li>
+            <li><img src="" alt="html" /> HTML</li>
             <li><img src={Css} alt="css" /> CSS</li>
             <li><img src={JavaScript} alt="JavaScript" />JavaScript</li>
             <li><img src={Bootstrap} alt="Bootstrap" />Bootstrap</li>
-            <li><img src={TypeScript} alt="TypeScript" /> TypeScript</li>
+            <li><img src="" alt="TypeScript" /> TypeScript</li>
             <li> <img src={React} alt="React" />React</li>
             <li> <img src={Vite} alt="Vite" />Vite</li>
             <li> <img src={Git} alt="Git" />Git</li>
             <li> <img src={Github} alt="GitHub" />GitHub</li>
             <li><img src={Responsive} alt="Design" /> Responsive design</li>
-            <li><img src={Figma} alt="FIGMA" /> Figma</li>
+            <li><img src="" alt="FIGMA" /> Figma</li>
           </ul>
         </div>
 
